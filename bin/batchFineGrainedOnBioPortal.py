f524c1d124dd093966638f4793ef228a7a244c86
Mostafa Sakr <Mostafa.Sakr@manchester.ac.uk>
2022-01-15 01:25:14 +0200

    readme

---
+++ bin/batchFineGrainedOnBioPortal.py
@@ -0,0 +1,89 @@
+import sys
+import os
+import subprocess
+import shutil
+from shutil import copyfile
+
+java = "/usr/bin/java"
+jar = "./restclient-dep-1.0.jar"
+repository = "../restclient/ontologies/bioportal/data"
+outRep = "../experiment"
+ontologyName = ""
+experimentNo = 1
+
+def clearDirectory(dir):
+    for file in os.listdir(dir):
+        file_path = os.path.join(dir, file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path): shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
+
+def makeDirectory(dir):
+    try:
+        os.makedirs(dir)
+    except FileExistsError:
+        print ("Output directory {dir} exists. Directory will be emptied and recycled".format(dir=outDir))
+
+def experiment(outDir, name, ont, sig):
+    experimentScript = "../restclient/src/main/python/fineGrainedOnBioportal.py"
+    makeDirectory(outDir)
+    clearDirectory(outDir)
+    print(name + " started")
+    executorCmd = "/Users/mostafa/anaconda3/bin/python {script} -d {dir} -n {name} -o {ontology} -s {sig}".format(script=experimentScript, dir=outDir, name=name, ontology=ont, sig=sig)
+    subprocess.call(executorCmd, shell=True)
+    print(name + " complete")
+
+print("Run with 10 percent of signature")
+for file in os.listdir(repository):
+    if(experimentNo > 0):
+        if(experimentNo < 50):
+            name = "experiment" + str(experimentNo)
+            ontology = os.path.join(repository, file)
+            outDir = outRep + name
+            experiment(outDir, name, ontology, 10)
+    experimentNo = experimentNo + 1
+
+experimentNo = 50
+print("Run with 30 percent of signature")
+for file in os.listdir(repository):
+    if(experimentNo > 49):
+        if(experimentNo < 100):
+            name = "experiment" + str(experimentNo)
+            ontology = os.path.join(repository, file)
+            outDir = outRep + name
+            experiment(outDir, name, ontology, 30)
+    experimentNo = experimentNo + 1
+
+experimentNo = 100
+print("Run with 50 percent of signature")
+for file in os.listdir(repository):
+    if experimentNo < 150:
+        if experimentNo > 99:
+            name = "experiment" + str(experimentNo)
+            ontology = os.path.join(repository, file)
+            outDir = outRep + name
+            experiment(outDir, name, ontology, 50)
+    experimentNo = experimentNo + 1
+
+summaryPath = outRep + "/summary.csv"
+if os.path.isfile(summaryPath):
+    os.unlink(summaryPath)
+print("Creating new summary at " + summaryPath)
+summaryFile = open(summaryPath, "w")
+summaryFile.write(
+    "Experiment,Ontology Size,Forgetting Signature Size,Deductive View Size,Lethe Size,Fame Time,Lethe Time,Final Time,Semantic Time,Semantic DE Time,Semantic Simplification Time,Reduction Time,Deductive DE Time,Deductive Simplification Time, No. Introduced Definers, No. Cyclic Definers, No. Delta Definers, Delta Size")
+summaryFile.close()
+
+
+
+summaryCmd = '{java} -cp {jar} {prog} -experiments {repo} -outFile {outFile}'.format(java=java,
+                                                                              jar=jar,
+                                                                              prog="pc1.pc2.pc3.SemanticToDeductiveSummaryGenerator",
+                                                                              repo=outRep,
+                                                                              outFile=summaryPath)
+
+print(summaryCmd, flush=True)
+returned_value = subprocess.call(summaryCmd, shell=True)
---
+++ bin/fineGrainedOnBioportal.py
@@ -0,0 +1,114 @@
+import subprocess
+from subprocess import Popen, TimeoutExpired
+import glob
+import random
+import os
+import json
+import requests
+import shutil
+import sys
+import getopt
+import datetime
+
+def printStartDate():
+    now = datetime.datetime.now()
+    print (now.strftime("Process started at %Y-%m-%d %H:%M:%S"))
+
+def printEndDate():
+    now = datetime.datetime.now()
+    print (now.strftime("Process ended at %Y-%m-%d %H:%M:%S"))
+
+java = "/usr/bin/java"
+jar = "./restclient-dep-1.0.jar"
+fameJar = "./fame-dep-1.0.jar"
+letheJar = "./lethe-dep-1.0.jar"
+signature = "100"
+experiment = ""
+ontology = ""
+
+argv = sys.argv[1:]
+opts, args = getopt.getopt(argv, "hd:n:o:s:", ["outDir=", "exName=", "ont=", "sig="])
+for opt, arg in opts:
+    if opt in ("-d", "--outDir"):
+        outDir = arg
+    elif opt in ("-n", "--exName"):
+        experimentName = arg
+    elif opt in ("-o", "--ont"):
+        ontology = arg
+    elif opt in ("-s", "--sig"):
+        signature = arg
+    else:
+        print('semanticforgettingexperiment.py -n <experimentName> -c <signatureCoverage>')
+        sys.exit()
+
+################################################################################################################
+
+ontologyOWL = "{outDir}/Ontology.owl".format(outDir=outDir)
+ontologyClausal = "{outDir}/Ontology.clausal".format(outDir=outDir)
+sigFile = "{outDir}/signature".format(outDir=outDir)
+
+printStartDate()
+########################################################Plan Experiment##############################################
+plannerCmd = '{java} -cp {jar} {prog} -ontology {ontology} -signature {signature} -outDir {outDir} -quantifiedSignature'.format(java=java,
+                                                                                                             jar=jar,
+                                                                                                             prog="pc1.pc2.pc3.ExperimentPlannerWithSignaturePortion",
+                                                                                                             ontology=ontology,
+                                                                                                             outDir=outDir,
+                                                                                                             signature=signature)
+
+if not (os.path.exists(ontologyOWL) and os.path.exists(ontologyClausal) and os.path.exists(sigFile)):
+    print("Preparing Experiment")
+    print(plannerCmd, flush=True)
+    with open(outDir + '/ExperimentPlan.log', 'w') as out:
+        subprocess.call(plannerCmd, stdout=out, stderr=out, shell=True)
+    printEndDate()
+
+############################################Fame##############################################################
+
+fameCmd = '{java} -cp {jar} {prog} -ont {ont} -outDir {outDir} -sig {signature}'.format(java=java,
+                                                                                        jar=fameJar,
+                                                                                        prog="pc1.pc2.pc3.ForgetFame",
+                                                                                        ont=ontologyOWL,
+                                                                                        outDir=outDir,
+                                                                                        signature=sigFile)
+
+# if not os.path.exists(outDir + '/' + "Fame.time"):
+#     print(fameCmd, flush=True)
+#     with open(outDir + '/Fame.log', 'w') as out:
+#         subprocess.call(fameCmd, stdout=out, stderr=out, shell=True)
+#     printEndDate()``
+
+############################################Lethe##############################################################
+
+letheCmd = '{java} -cp {jar} {prog} -ont {ont} -outDir {outDir} -sig {signature}'.format(java=java,
+                                                                                        jar=letheJar,
+                                                                                        prog="pc1.pc2.pc3.ForgetLethe",
+                                                                                        ont=ontologyOWL,
+                                                                                        outDir=outDir,
+                                                                                        signature=sigFile)
+
+if not os.path.exists(outDir + '/' + "Lethe.time"):
+    print(letheCmd, flush=True)
+    with open(outDir + '/Lethe.log', 'w') as out:
+        subprocess.call(letheCmd, stdout=out, stderr=out, shell=True)
+    printEndDate()
+
+############################################Semantic Forgetting##############################################################
+newRunDir = outDir + "/newRun"
+try:
+    os.makedirs(newRunDir)
+except FileExistsError:
+    
+executorCmd = "{java} -cp {jar} {prog} -outDir {outDir}/newRun -sig {sig} -ont {ont} -reduceToDeductive -verbose".format(
+    java=java,
+    jar=jar,
+    prog="pc1.pc2.pc3.SemanticForgetting",
+    outDir=outDir,
+    sig=sigFile,
+    ont=ontologyClausal)
+
+if not os.path.exists(outDir + '/newRun/' + "time"):
+    print(executorCmd, flush=True)
+    with open(outDir + '/SemanticToDeductive.log', 'w') as out:
+        subprocess.call(executorCmd, stdout=out, stderr=out, shell=True)
+    printEndDate()
